71c608d2524f1d226a900aed739d9a8e94c69966
lucky1896 <ahmedmustafa18960@gmail.com>
2026-07-11 09:06:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
+app.get('/', (req, res) => res.sendFile('earny-day.html', { root: 'public' }));
 
 // ---------- DATABASE (Supabase Postgres) ----------
 const pool = new Pool({
